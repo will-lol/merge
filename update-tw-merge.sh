ec7238800425c54c042768851720c56aea073360
William Bradshaw <65345408+will-lol@users.noreply.github.com>
2024-02-27 17:43:27 +1100

    ensure update-tw-merge.sh fetches latest tw-merge

--- update-tw-merge.sh
+++ update-tw-merge.sh
@@ -1,1 +1,1 @@
-curl -L https://cdn.jsdelivr.net/npm/tailwind-merge@2.2.1/dist/bundle-cjs.min.js > tw-merge.js
+curl -L https://cdn.jsdelivr.net/npm/tailwind-merge@latest/dist/bundle-cjs.min.js > tw-merge.js
