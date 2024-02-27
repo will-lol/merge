b94e1cc7af5a686d2ff6f4bf56cac74cf9122e5c
will <will.bradshaw50@gmail.com>
2024-02-27 20:27:03 +1100

    fix javy (massive hack)

--- flake.nix
+++ flake.nix
@@ -6,27 +6,24 @@
   };
 
   outputs = { self, nixpkgs }:
-      let 
-        system = "x86_64-linux";
-        overlays = [];
-        lib = nixpkgs.lib;
-
-        javyCli = pkgs.stdenv.mkDerivation {
+    let 
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      overlays = [ (final: prev: {
+        javy = pkgs.stdenv.mkDerivation {
           name = "javy";
           version = "1.4.0";
           src = pkgs.fetchurl {
             url = "https://github.com/bytecodealliance/javy/releases/download/v1.4.0/javy-x86_64-linux-v1.4.0.gz";
             hash = "sha256-SfBLy1Q7OlHf/7Qsv9cVmpHNPGgNVxtBvojBmSzx0vI=";
-            postFetch = ''
-              cp $out src.gz
-              ${pkgs.gzip}/bin/gzip -c -d src.gz > $out
-            '';
           };
-
           nativeBuildInputs = with pkgs; [ gzip autoPatchelfHook ];
-          buildInputs = with pkgs; [ openssl glibc gcc-unwrapped ];
-          phases = [ "installPhase" ];
+          buildInputs = with pkgs; [ openssl glibc gcc-unwrapped zlib ];
+          unpackPhase = ''
+            echo $src
+          '';
           installPhase = ''
+            echo $unpackPhase
             runHook preInstall
             install -m755 -D $src $out/bin/javy
             runHook postInstall
@@ -35,17 +32,18 @@
             platforms = platforms.linux;
           };
         };
-        pkgs = import nixpkgs { inherit system overlays; };
-      in
-        {
-          packages = {
-          };
-          # defaultPackage = example;
-          devShell.${system} = pkgs.mkShell {
-            packages = [ pkgs.go pkgs.gopls javyCli ];
-            shellHook = ''
-            '';
-          };
-        }
-    ;
+      }) ];
+
+      pkgs = import nixpkgs { inherit system overlays; };
+    in
+      {
+        packages = {
+        };
+        # defaultPackage = example;
+        devShell.${system} = pkgs.mkShell {
+          packages = with pkgs; [ go gopls javy ];
+          shellHook = ''
+          '';
+        };
+      };
 }
