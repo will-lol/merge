6df81a2d92bec954050a660a783e1327f0b0b2e7
will <will.bradshaw50@gmail.com>
2024-02-27 16:17:15 +1100

    tabs to spaces for readability

--- flake.nix
+++ flake.nix
@@ -1,50 +1,51 @@
 {
-	description = "";
+  description = "";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
 
-	outputs = { self, nixpkgs }:
-			let 
-				system = "x86_64-linux";
-				overlays = [];
-				lib = nixpkgs.lib;
-				javyCli = pkgs.stdenv.mkDerivation {
-					name = "javy";
-					version = "1.4.0";
-					src = pkgs.fetchurl {
-						url = "https://github.com/bytecodealliance/javy/releases/download/v1.4.0/javy-x86_64-linux-v1.4.0.gz";
-						hash = "sha256-SfBLy1Q7OlHf/7Qsv9cVmpHNPGgNVxtBvojBmSzx0vI=";
-						postFetch = ''
-							cp $out src.gz
-							${pkgs.gzip}/bin/gzip -c -d src.gz > $out
-						'';
-					};
-					phases = [ "installPhase" "patchPhase" ];
-					sourceRoot = ".";
-					nativeBuildInputs = with pkgs; [ gzip autoPatchelfHook ];
-					buildInputs = with pkgs; [ openssl glibc gcc-unwrapped ];
-					installPhase = ''
-						runHook preInstall
-						install -m755 -D $src $out/bin/javy
-						runHook postInstall
-					'';
-					meta = with lib; {
-						platforms = platforms.linux;
-					};
-				};
-				pkgs = import nixpkgs { inherit system overlays; };
-			in
-				{
-					packages = {
-					};
-					# defaultPackage = example;
-					devShell.${system} = pkgs.mkShell {
-						packages = [ pkgs.go pkgs.gopls javyCli ];
-						shellHook = ''
-						'';
-					};
-				}
-		;
+  outputs = { self, nixpkgs }:
+      let 
+        system = "x86_64-linux";
+        overlays = [];
+        lib = nixpkgs.lib;
+
+        javyCli = pkgs.stdenv.mkDerivation {
+          name = "javy";
+          version = "1.4.0";
+          src = pkgs.fetchurl {
+            url = "https://github.com/bytecodealliance/javy/releases/download/v1.4.0/javy-x86_64-linux-v1.4.0.gz";
+            hash = "sha256-SfBLy1Q7OlHf/7Qsv9cVmpHNPGgNVxtBvojBmSzx0vI=";
+            postFetch = ''
+              cp $out src.gz
+              ${pkgs.gzip}/bin/gzip -c -d src.gz > $out
+            '';
+          };
+
+          nativeBuildInputs = with pkgs; [ gzip autoPatchelfHook ];
+          buildInputs = with pkgs; [ openssl glibc gcc-unwrapped ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            runHook preInstall
+            install -m755 -D $src $out/bin/javy
+            runHook postInstall
+          '';
+          meta = with lib; {
+            platforms = platforms.linux;
+          };
+        };
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+        {
+          packages = {
+          };
+          # defaultPackage = example;
+          devShell.${system} = pkgs.mkShell {
+            packages = [ pkgs.go pkgs.gopls javyCli ];
+            shellHook = ''
+            '';
+          };
+        }
+    ;
 }
