0b5c783b00ed6809eaf3b7fdc302aaded36cdf49
will <will.bradshaw50@gmail.com>
2024-02-27 20:35:24 +1100

    fix javy install for real this time

--- flake.nix
+++ flake.nix
@@ -15,17 +15,19 @@
           version = "1.4.0";
           src = pkgs.fetchurl {
             url = "https://github.com/bytecodealliance/javy/releases/download/v1.4.0/javy-x86_64-linux-v1.4.0.gz";
-            hash = "sha256-SfBLy1Q7OlHf/7Qsv9cVmpHNPGgNVxtBvojBmSzx0vI=";
+            hash = "sha256-NZIzT8BdtgKiE3RbePWEY1E5TWe9mr2LSRhhmxzWzd8=";
           };
           nativeBuildInputs = with pkgs; [ gzip autoPatchelfHook ];
           buildInputs = with pkgs; [ openssl glibc gcc-unwrapped zlib ];
           unpackPhase = ''
-            echo $src
+            cp $src src.gz 
+            mkdir $out
+            ${pkgs.gzip}/bin/gzip -c -d src.gz > $out/javy
           '';
           installPhase = ''
             echo $unpackPhase
             runHook preInstall
-            install -m755 -D $src $out/bin/javy
+            install -m755 -D $out/javy $out/bin/javy
             runHook postInstall
           '';
           meta = with lib; {
