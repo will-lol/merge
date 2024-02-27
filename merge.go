14941b1a0b65f698bfb9ecbe9ed5dec3fa2f35e6
will <will.bradshaw50@gmail.com>
2024-02-27 12:59:57 +1100

    add basic merging and tests

--- merge.go
+++ merge.go
@@ -1,17 +1,63 @@
 package merge
 
-import ()
-
 type Merger interface {
+	Merge(existing Attrs, incoming Attrs) Attrs
 }
 
 type merger struct {
+	AttrsFuncMap map[string][]MergeFunc
 }
 
 type Attrs map[string]any
 
-type MergeFunc func(existing Attrs, incoming Attrs) (comitted Attrs, remaining Attrs)
+type MergeFunc func(existing any, incoming any) (committed any, remaining any)
 
-func New(attrsMap map[string]MergeFunc) Merger {
-	return &merger{}
+func New(attrsFuncMap map[string][]MergeFunc) Merger {
+	for _, funcs := range attrsFuncMap {
+		funcs = append(funcs, DefaultMergeFunc)
+	}
+
+	return &merger{
+		AttrsFuncMap: attrsFuncMap,
+	}
+}
+
+func DefaultMergeFunc(existing any, incoming any) (remaining any, committed any) {
+	return nil, incoming
+}
+
+func isFullyMerged(remaining any) bool {
+	return remaining == nil || remaining == ""
+}
+
+func (m merger) Merge(existing Attrs, incoming Attrs) Attrs {
+	for key, existingValue := range existing {
+		incomingValue, ok := incoming[key]
+
+		if ok {
+			mergeFuncs, ok := m.AttrsFuncMap[key]
+
+			var remaining, committed any
+			remaining = existingValue
+			committed = incomingValue
+
+			if ok {
+				for _, mergeFunc := range mergeFuncs {
+					if !isFullyMerged(remaining) {
+						remaining, committed = mergeFunc(remaining, committed)
+					} else {
+						break
+					}
+				}
+				incoming[key] = committed
+			} else {
+				remaining, committed = DefaultMergeFunc(remaining, committed)
+				incoming[key] = committed
+			}
+		} else {
+			incoming[key] = existingValue
+		}
+	}
+
+	return incoming
 }
--- merge_test.go
+++ merge_test.go
@@ -1,5 +1,24 @@
 package merge
 
 import (
-
+	"fmt"
+	"testing"
 )
+
+func TestMergeSimple(t *testing.T) {
+	m := New(map[string][]MergeFunc{})
+	res := m.Merge(Attrs{ "class": "px-5" }, Attrs{ "id": "button" })
+	fmt.Println(res)
+	if (res["id"] != "button" && res["class"] != "px-5") {
+		t.Fatalf("Needed id=button and class=px-5 but got %v", res)
+	}
+}
+
+func TestMergeCollision(t *testing.T) {
+	m := New(map[string][]MergeFunc{})
+	res := m.Merge(Attrs{ "class": "px-5" }, Attrs{ "class": "mt-5" })
+	fmt.Println(res)
+	if (res["class"] != "mt-5") {
+		t.Fatalf("Needed class=mt-5 but got %v", res)
+	}
+}
