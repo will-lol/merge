bc1062136f7f7b7f5ac881195e720578d69ba21e
will <will.bradshaw50@gmail.com>
2024-02-28 11:57:23 +1100

    remove twMergeWasm as it is too slow

--- twMergeWasm/updateWasm.sh
+++
@@ -1,5 +0,0 @@
-pushd lib
-curl -L https://cdn.jsdelivr.net/npm/tailwind-merge@latest/dist/bundle-mjs.mjs > tw-merge.js
-esbuild index.js --bundle --outfile=bundle.js
-javy compile bundle.js -o index.wasm
-popd
