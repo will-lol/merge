134ee4c29c3a4a54e422266910b92656fd67b6f2
will <will.bradshaw50@gmail.com>
2024-02-28 13:03:12 +1100

    go fmt

--- merge_test.go
+++ merge_test.go
@@ -7,18 +7,18 @@ import (
 
 func TestMergeSimple(t *testing.T) {
 	m := New(map[string][]MergeFunc{})
-	res := m.Merge(Attrs{ "class": "px-5" }, Attrs{ "id": "button" })
+	res := m.Merge(Attrs{"class": "px-5"}, Attrs{"id": "button"})
 	fmt.Println(res)
-	if (res["id"] != "button" && res["class"] != "px-5") {
+	if res["id"] != "button" && res["class"] != "px-5" {
 		t.Fatalf("Needed id=button and class=px-5 but got %v", res)
 	}
 }
 
 func TestMergeCollision(t *testing.T) {
 	m := New(map[string][]MergeFunc{})
-	res := m.Merge(Attrs{ "class": "px-5" }, Attrs{ "class": "mt-5" })
+	res := m.Merge(Attrs{"class": "px-5"}, Attrs{"class": "mt-5"})
 	fmt.Println(res)
-	if (res["class"] != "mt-5") {
+	if res["class"] != "mt-5" {
 		t.Fatalf("Needed class=mt-5 but got %v", res)
 	}
 }
