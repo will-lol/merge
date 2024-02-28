3bb0308a06729344e5c91f6ba5c666773b6d2f65
will <will.bradshaw50@gmail.com>
2024-02-28 12:38:42 +1100

    add tw-merge attribution

--- tw-merge.js
+++ tw-merge.js
@@ -1,8 +1,3 @@
-pushd lib
-curl -L https://cdn.jsdelivr.net/npm/tailwind-merge@latest/dist/es5/bundle-mjs.mjs > tw-merge.js
-esbuild tw-merge.js --target=es5 --bundle --outfile=bundle.js --global-name=m
-cat <<HERE >> bundle.js 
-
 /* MIT License
 
 Copyright (c) 2021 Dany Castillo
@@ -12,5 +7,8 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
-HERE
+
+EOF 
+curl -L https://cdn.jsdelivr.net/npm/tailwind-merge@latest/dist/es5/bundle-mjs.mjs >> tw-merge.js
+esbuild tw-merge.js --target=es5 --bundle --outfile=bundle.js --global-name=m
 popd
